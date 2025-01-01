bf6e1ee153bc37365e31cbcf7731dcf0ea16660b
Alicia <arosebures@gmail.com>
2025-01-01 16:45:31 -0500

    Completed part one of assignment

--- js/index.js
+++ js/index.js
@@ -1,4 +1,27 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+console.log (1+1)
+console.log (1-1)
+console.log (1+"1")
+console.log (1+"2")
+console.log (1+ +"1")
 
+console.log (3-4*5)
+console.log ((3-4)*5)
+
+console.log (6/5)
+console.log (6%5)
+console.log (17%4)
+console.log (18%4)
+
+console.log (1==1)
+console.log (1=="1")
+console.log (1==="1")
+console.log (1===1)
+console.log (1===5)
+
+console.log (1 || 2)
+console.log (2 || 1)
+console.log (1&&2)
+console.log (1&&1)
